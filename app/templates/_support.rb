768c96a559cb53c030d7e77e9916d05a5e510f4b
doodzik <4004blog@gmail.com>
2014-10-22 22:33:58 +0200

    add loadpath to support file

--- app/templates/_support.rb
+++ app/templates/_support.rb
@@ -1,1 +1,5 @@
+$LOAD_PATH.unshift(File.join(File.join(File.dirname(__FILE__), '..'), 'contracts'))
+$LOAD_PATH.unshift(File.join(File.join(File.dirname(__FILE__), '..'), 'adapters'))
+$LOAD_PATH.unshift(File.join(File.join(File.dirname(__FILE__), '..'), 'lib'))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..'))
 
